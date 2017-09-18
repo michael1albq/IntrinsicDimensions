b906197f478c34e70f528a6bc25893ac2f9c509b
kmok002 <kafungmok002@gmail.com>
2017-09-17 23:21:20 -0400

    Update sample_line.py

--- sample_line.py
+++ sample_line.py
@@ -7,7 +7,7 @@ Created on Thu Aug 31 22:05:55 2017
 
 import numpy as np
 
-
+# Outputs n sample points from a random line in R^3 as a matrix of the form nx3. 
 def sample_line(n):
 
     dir_vec = np.random.randn(3)
