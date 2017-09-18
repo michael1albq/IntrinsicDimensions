3522f8629fb428987dd373d88b31c3718ec85402
kmok002 <kafungmok002@gmail.com>
2017-09-17 23:20:22 -0400

    Update sample_sphere.py
    Added comments and recenter the sphere.

--- sample_sphere.py
+++ sample_sphere.py
@@ -6,11 +6,15 @@ Created on Wed Sep 13 11:07:48 2017
 """
 
 import numpy as np
+# Outputs n sample points from a random sphere in R^3 centered at c as a matrix of the form nx3.
+#Note: We use the last construction found in http://mathworld.wolfram.com/SpherePointPicking.html.
 
-
-def sample_sphere(n):
+def sample_sphere(n, c):
     vec = np.random.randn(3, n)
     vec /= np.linalg.norm(vec, axis=0)
+    recenter = np.repeat(c,n)
+    recenter = np.reshape(recenter, (3,n))
+    vec = vec + recenter
     return np.transpose(vec)
 
 if __name__ == "__main__":
