6f532d8a2e675cd8e7edeb0aac14dea80d8d1311
“Michael <mike20202@gmail.com>
2017-09-18 01:36:36 -0400

    Added comments in sample lin/plane/sphere, added readme

--- sample_line.py
+++ sample_line.py
@@ -7,16 +7,25 @@ Created on Thu Aug 31 22:05:55 2017
 
 import numpy as np
 
-# Outputs n sample points from a random line in R^3 as a matrix of the form nx3. 
-def sample_line(n):
 
+def sample_line(n):
+    """
+    Outputs n sample points from a random line in R^3 as a matrix of the form nx3
+    :param n: No. of points to be sampled
+    :return: Coordinates of the n points on the line [x,y,z]
+    """
+    # Generate direction vector to be scaled
     dir_vec = np.random.randn(3)
     line_point = np.random.randn(3)
+
+    # Generate random scalars
     parameter = np.transpose(np.random.randn(n)[np.newaxis])
-    line_points = line_point + parameter*dir_vec
+
+    # Translate dir_vec by random scalars to get points on the same line
+    line_points = line_point + parameter * dir_vec
 
     return line_points
 
+
 if __name__ == "__main__":
     print(sample_line(10))
-
--- sample_sphere.py
+++ sample_sphere.py
@@ -6,16 +6,24 @@ Created on Wed Sep 13 11:07:48 2017
 """
 
 import numpy as np
-# Outputs n sample points from a random sphere in R^3 centered at c as a matrix of the form nx3.
-#Note: We use the last construction found in http://mathworld.wolfram.com/SpherePointPicking.html.
+
 
 def sample_sphere(n, c):
+    """
+    Outputs sample points from a random sphere in R^3 centered at c as a matrix of the form nx3
+
+    :param n: No. of points to be sampled
+    :param c: Vector - Coordinates for Center of sphere [x,y,z]
+    :return: Coordinates of the n points on the sphere [x,y,z]
+
+    Note: We use the last construction found in http://mathworld.wolfram.com/SpherePointPicking.html
+    """
     vec = np.random.randn(3, n)
     vec /= np.linalg.norm(vec, axis=0)
-    recenter = np.repeat(c,n)
-    recenter = np.reshape(recenter, (3,n))
+    recenter = np.repeat(c, n)
+    recenter = np.reshape(recenter, (3, n))
     vec = vec + recenter
     return np.transpose(vec)
 
 if __name__ == "__main__":
-    print(sample_sphere(10))
+    print(sample_sphere(10, [-0.45037415,  0.75855613,  2.88560308]))
